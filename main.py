28ff7695d1b5654069a294b98409d161a221b6d9
tranquilo-01 <kdziech@student.agh.edu.pl>
2024-01-13 17:19:41 +0100

    added basis_function_derivative

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import numpy as np
+
 domain_left = 0
-domain_right = 10
+domain_right = 2
 domain_length = domain_right - domain_left
 precision = 10
+h = domain_length / precision
+h_inv = 1 / h
 
 
 def integrate_trapezoidal(function, lower_bound, upper_bound, iterations):
@@ -29,15 +32,11 @@ def E(x):
 
 def basis_function_value(i, x):
     """
-    Function returns a
     :param i: number of finite element
     :param x: point on x-axis
     :return: value of the basis function associated with i-th finite element at point x
     """
-    h = domain_length / precision
-    h_inv = precision / domain_length
-
-    center = domain_length * i / precision
+    center = domain_length * i / precision + domain_left
     left = center - h
     right = center + h
 
@@ -48,9 +47,23 @@ def basis_function_value(i, x):
     return (right - x) * h_inv
 
 
+def basis_function_derivative(i, x):
+    """
+    :param i: number of finite element
+    :param x: point on x-axis
+    :return: derivative value of the basis function associated with i-th finite element at point x
+    """
+    center = domain_length * i / precision + domain_left
+    left = center - h
+    right = center + h
+
+    if x < left or x > right:
+        return 0.0
+    if x <= center:
+        return h_inv
+    return -h_inv
+
+
 if __name__ == "__main__":
     for v in np.linspace(domain_left, domain_right, 100):
-        print(basis_function_value(10, v))
-
-
-
+        print(basis_function_value(1, v), basis_function_derivative(1, v))
