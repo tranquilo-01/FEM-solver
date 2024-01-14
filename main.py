c51249e55cca9c5bafcc27342b29a04714069552
tranquilo-01 <kdziech@student.agh.edu.pl>
2024-01-14 15:31:33 +0100

    cleanup done

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import scipy.integrate as integrate
 domain_left = 0
 domain_right = 2
 domain_length = domain_right - domain_left
-# FIXME some combinations of fe_number and integration_step give weird results
+# some combinations of fe_number and integration_step give weird results
 # mostly occurs when fe_number is big and integration step is also big
 # number of finite elements, must be greater than 2
 fe_number = 32
 integration_step = 1 / 1000
-asc_slope_basis = domain_length / fe_number
-desc_slope_basis = 1 / asc_slope_basis
+fe_step = domain_length / fe_number
+fe_step_inv = 1 / fe_step
 
 
 def integrate_trapezoidal(f, lower_bound, upper_bound):
@@ -34,20 +34,19 @@ Function performs numerical integration using trapezoidal rule.
             b = f(i + integration_step)
             integral_value += (a + b) * integration_step / 2
         i += integration_step
-
-    # scipy_value = integrate.quad(f, lower_bound, upper_bound)
-
-    # print("my value: ", integral_value, "scipy value: ", scipy_value, "diff: ", abs(integral_value - scipy_value[0]))
     return integral_value
 
 
 def E(x):
+    """
+    elasticity function given in the assignment
+    :param x: x value
+    :return:  E(x)
+    """
     if 0 <= x <= 1:
         return 3
     elif 1 < x <= 2:
         return 5
-    else:
-        pass
 
 
 def basis_function_value(i, x):
@@ -57,14 +56,14 @@ def basis_function_value(i, x):
     :return: value of the basis function associated with i-th finite element at point x
     """
     center = domain_length * i / fe_number + domain_left
-    left = center - asc_slope_basis
-    right = center + asc_slope_basis
+    left = center - fe_step
+    right = center + fe_step
 
     if x < left or x > right:
         return 0.0
     if x <= center:
-        return (x - left) * desc_slope_basis
-    return (right - x) * desc_slope_basis
+        return (x - left) * fe_step_inv
+    return (right - x) * fe_step_inv
 
 
 def basis_function_derivative(i, x):
@@ -74,62 +73,14 @@ def basis_function_derivative(i, x):
     :return: derivative value of the basis function associated with i-th finite element at point x
     """
     center = domain_length * i / fe_number + domain_left
-    left = center - asc_slope_basis
-    right = center + asc_slope_basis
+    left = center - fe_step
+    right = center + fe_step
 
     if x < left or x > right:
         return 0.0
     if x <= center:
-        return desc_slope_basis
-    return -desc_slope_basis
-
-
-def plot(result):
-    x = np.linspace(domain_left, domain_right, fe_number + 1)
-    plt.plot(x, result)
-    plt.xlabel("x")
-    plt.ylabel("u(x)")
-    plt.title("Odkształcenie sprężyste")
-    info_box = f"Liczba elementów skończonych: {fe_number}\nKrok całkowania: {integration_step}"
-    plt.annotate(info_box, xy=(0.58, 0.91), xycoords='axes fraction', fontsize=8, bbox=dict(boxstyle="round,pad=0.3", edgecolor="black", facecolor="white"))
-    plt.grid(True)
-    plt.yticks(np.arange(result.min(), result.max()+1, 2))
-    plt.show()
-
-
-def solve():
-    # Build B matrix
-    b_matrix = np.zeros((fe_number, fe_number))
-
-    for n in range(fe_number):
-        for m in range(fe_number):
-            integral = 0
-
-            if abs(m - n) <= 1:
-                integrate_from = domain_length * max(max(n, m) - 1, 0) / fe_number
-                integrate_to = domain_length * min(min(n, m) + 1, fe_number) / fe_number
-
-                def integrand(x): return E(x) * basis_function_derivative(n, x) * basis_function_derivative(m, x)
-
-                integral = integrate_trapezoidal(integrand, integrate_from, integrate_to)
-                # integral, _ = integrate.quad(integrand, integrate_from, integrate_to)
-
-            b_matrix[n, m] = -E(0) * basis_function_value(n, 0) * basis_function_value(m, 0) + integral
-
-    # Build L vector
-    l_vector = np.zeros(fe_number)
-    l_vector[0] = -10 * E(0) * basis_function_value(0, 0)
-
-    # print(b_matrix)
-    # print(l_vector)
-
-    # Calculate coefficients
-    # coefficients = np.linalg.solve(b_matrix, l_vector)
-    coefficients = gaussian_elimination(b_matrix, l_vector)
-
-    result = np.concatenate((coefficients, [0]))
-    # print(result)
-    return result
+        return fe_step_inv
+    return -fe_step_inv
 
 
 def gaussian_elimination(matrix, vector):
@@ -157,15 +108,49 @@ def gaussian_elimination(matrix, vector):
     return res
 
 
-def test_integration():
-    print("x^2")
-    integrate_trapezoidal(lambda x: x * x, -1.2137, 2)
-    print("3x^3 + 5x^2 + 2x - 8")
-    integrate_trapezoidal(lambda x: 3 * x ** 3 + 5 * x ** 2 + 2 * x - 8, -1.2137, 2)
-    print("50x")
-    integrate_trapezoidal(lambda x: 50 * x, -1.2137, 2)
-    print("500")
-    integrate_trapezoidal(lambda x: 500, -1.2137, 2)
+def plot(result):
+    x = np.linspace(domain_left, domain_right, fe_number + 1)
+    plt.plot(x, result)
+    plt.xlabel("x")
+    plt.ylabel("u(x)")
+    plt.title("Odkształcenie sprężyste")
+    info_box = f"Liczba elementów skończonych: {fe_number}\nKrok całkowania: {integration_step}"
+    plt.annotate(info_box, xy=(0.58, 0.91), xycoords='axes fraction', fontsize=8, bbox=dict(boxstyle="round,pad=0.3", edgecolor="black", facecolor="white"))
+    plt.grid(True)
+    plt.yticks(np.arange(result.min(), result.max()+1, 2))
+    plt.show()
+
+
+def solve():
+    """
+    function builds a linear equation system connected to elastic deformation FEM problem
+    given in the assignment and solves it
+    :return: vector of finite elements coefficients
+    """
+    b_matrix = np.zeros((fe_number, fe_number))
+
+    for i in range(fe_number):
+        for j in range(fe_number):
+            if abs(j - i) <= 1:
+                integrate_from = domain_length * max(max(i, j) - 1, 0) / fe_number
+                integrate_to = domain_length * min(min(i, j) + 1, fe_number) / fe_number
+
+                def integrand(x): return E(x) * basis_function_derivative(i, x) * basis_function_derivative(j, x)
+
+                # integrate E(x)du/dx*dv/dx dx -3u(0)v(0)
+                b_matrix[i, j] = integrate.quad(integrand, integrate_from, integrate_to)[0] - 3 * basis_function_value(i, 0) * basis_function_value(j, 0)
+                # b_matrix[i, j] = integrate_trapezoidal(integrand, integrate_from, integrate_to) - 3 * basis_function_value(i, 0) * basis_function_value(j, 0)
+
+    l_vector = np.zeros(fe_number)
+    # -30v(0)
+    l_vector[0] = -30 * basis_function_value(0, 0)
+
+    # coefficients = np.linalg.solve(b_matrix, l_vector)
+    coefficients = gaussian_elimination(b_matrix, l_vector)
+
+    # u(2) = 0
+    result = np.concatenate((coefficients, [0]))
+    return result
 
 
 if __name__ == "__main__":
